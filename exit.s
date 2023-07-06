d0e9b07c98366e71fea242b2a2372d81f4005564
Naman Tamrakar <59503233+namantam1@users.noreply.github.com>
2023-07-06 19:44:33 +0530

    Update exit.s

--- exit.s
+++ exit.s
@@ -2,6 +2,9 @@
 # date: 06.07.2023
 # description: A sample program to exits with a certain exit code
 #              exit code can be in range [0, 255].
+# memory:
+#    %eax - store sys call
+#    %ebx - store exist status for this sys call
 
 
 .section .text
