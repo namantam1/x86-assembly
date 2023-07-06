8edf57268177d893213991ca10d8562f0d37936e
Naman Tamrakar <59503233+namantam1@users.noreply.github.com>
2023-07-06 19:18:24 +0530

    Update and rename exit_code.s to exit.s

---
+++ exit.s
@@ -0,0 +1,13 @@
+# auther: Naman Tamrakar
+# date: 06.07.2023
+# description: A sample program to exits with a certain exit code
+#              exit code can be in range [0, 255].
+
+
+.section .text
+.globl _start
+
+_start:
+    movl $1, %eax # sys call for exit
+    movl $10, %ebx # exit status code
+    int $0x80 # wake kernal to exit
